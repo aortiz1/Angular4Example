3b7af274f74e78ebf84eea2b59661bea270353ec
Armando Ortiz <aortiz@sciodev.com>
2018-07-26 14:10:15 -0500

    Fixed package dependencies
    Added navbar in main page

--- AngularCourse/src/app/app.module.ts
+++ AngularCourse/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AlgoComponent } from './algo/algo.component';
 import { LoginComponent } from './login/login.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,13 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
