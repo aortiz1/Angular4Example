b1ab4d2a385a40c2f86975cb7f50705dc18acccf
Armando Ortiz <aortiz@sciodev.com>
2018-07-26 11:28:59 -0500

    Updated npm , added component folder

--- AngularCourse/src/app/app.module.ts
+++ AngularCourse/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlgoComponent } from './algo/algo.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlgoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
